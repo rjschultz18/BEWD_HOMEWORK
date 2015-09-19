d6314f91c78b18f6a2a2811d09478114eb964674
Rachel Schultz <rjschultz18@gmail.com>
2015-09-18 23:07:50 -0700

    added who_am_i

---
+++ who_am_i.rb
@@ -0,0 +1,12 @@
+print "what is your name"
+your_name = gets.chomp.downcase
+print "where did you grow up"
+grow_up = gets.chomp.downcase
+print "what is your current occupation"
+occupation = gets.chomp.downcase
+print "what is your hidden talent"
+hidden_talent = gets.chomp.downcase
+
+puts "Your name is #{your_name}
+you grew up in #{grow_up} and your occupation is
+#{occupation}. Your hidden talent is #{hidden_talent}."
