7584c15b7e5d5ea7bfcb1ea3945f4691bb2ba33d
Rachel Schultz <rjschultz18@gmail.com>
2015-10-16 10:44:38 -0700

    recoded pants

--- class_9_10_lab/ruby_pantscast.rb
+++ class_9_10_lab/ruby_pantscast.rb
@@ -11,8 +11,16 @@ ziphip = HTTParty.get ("http://api.zippopotam.us/us/#{zip}")
   lat = ziphip['places'][0]['latitude']
   lng = ziphip['places'][0]['longitude']
 
-forecast = HTTParty.get ("https://api.forecast.io/forecast/b6ec16c6daf2eaa642175d3a5d80d219/#{lat},#{lng}")
+forecast = HTTParty.get ("https://api.forecast.io/forecast/017e4347b30c28eea9e0dad88f1f8ca1/#{lat},#{lng}")
   @current = forecast['currently']['temperature']
+  @high = forecast['daily']['data'][0]['temperatureMax']
+  @low = forecast['daily']['data'][0]['temperatureMin']
+
+if @high >= 75
+      @message = 'Time to wear shorts!'
+  else
+      @message = 'Time to wear pants!'
+  end
 
   erb :pantscast
 end
